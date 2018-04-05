5ec948d76a06a393ce53b40d5009d386cecb6066
IPav7 <pavinich1997@mail.ru>
2018-04-05 18:33:38 +0300

    dbhelper

---
+++ app/src/main/java/com/example/android/waitlist/data/WaitlistDBHelper.java
@@ -0,0 +1,37 @@
+package com.example.android.waitlist.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by pavin on 05.04.2018.
+ */
+
+public class WaitlistDBHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "waitlist.db";
+
+    private static final int DATABASE_VERSION = 1;
+
+    public WaitlistDBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String create_table = "CREATE TABLE " + WaitlistContract.WaitlistEntry.TABLE_NAME + " (" +
+                WaitlistContract.WaitlistEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                WaitlistContract.WaitlistEntry.COLUMN_GUEST_NAME + " TEXT NOT NULL" +
+                WaitlistContract.WaitlistEntry.COLUMN_PARTY_SIZE + " INTEGER NOT NULL" +
+                WaitlistContract.WaitlistEntry.COLUMN_TIMESTAMP + " TIMESTAMP DEFAULT CURRENT_TIMESTAMP" +
+                ");";
+        db.execSQL(create_table);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + WaitlistContract.WaitlistEntry.TABLE_NAME + ";");
+        onCreate(db);
+    }
+}
